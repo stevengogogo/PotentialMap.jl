5f1ab9181b418abf321de174030ef6f973367fbe
Documenter.jl <documenter@juliadocs.github.io>
2020-11-21 11:40:00 +0000

    build based on 84f016c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PotentialMap","category":"page"},{"location":"#PotentialMap","page":"Home","title":"PotentialMap","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PotentialMap]","category":"page"}]
+}
