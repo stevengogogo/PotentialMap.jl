723072e13129ef018cf9ac9f65af252bdfc088d3
stevengogogo <stevengogogo4321@gmail.com>
2021-01-04 14:04:48 +0800

    revise toml

--- src/PotentialMap.jl
+++ src/PotentialMap.jl
@@ -3,10 +3,6 @@ module PotentialMap
 using FindSteadyStates
 using SteadyStateDiffEq
 using LinearAlgebra
-<<<<<<< HEAD
-=======
-
->>>>>>> 0ad723ce3250776a20faeb03c0f8b5e1160b8d8d
 # Write your package code here.
 include("fixedpoint_gen.jl")
 include("DiffusionMatrix.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,5 @@
 using PotentialMap
-<<<<<<< HEAD
-using OrdinaryDiffEq
-=======
 using SteadyStateDiffEq
->>>>>>> 0ad723ce3250776a20faeb03c0f8b5e1160b8d8d
 using Test
 
 @testset "PotentialMap.jl" begin
